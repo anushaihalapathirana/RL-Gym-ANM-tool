65e9e2994d1b348d0fe945dbd922b07fe3740c4a
anushaihalapathirana <anusha.12@cse.mrt.ac.lk>
2021-07-30 04:13:05 -0400

    changes in mpc policy file

--- src/MPC/mpcPolicy.py
+++ src/MPC/mpcPolicy.py
@@ -2,6 +2,7 @@ import gym
 import numpy as np
 from gym_anm import MPCAgentConstant
 from testEnv import TestEnvironment
+import time
 
 def run(env, agent):
     env = env
@@ -15,16 +16,18 @@ def run(env, agent):
     best_score = env.reward_range[0]
     score_history = []
     score = 0
+    start_time = time
     # Run the policy.
     for step in range(n_games):
         state = env.reset()
         action = agent.act(env)
         new_state, reward, done, info = env.step(action)
-        print(f'step = {step}, reward = {reward:.3}')
+        # print(f'step = {step}, reward = {reward:.3}')
         score += reward
         state = new_state
 
     score_history.append(score)
     avg_score = np.mean(score_history[-100:])
-
+    end_time = time
     print('score %.2f' % score, 'trailing 100 games avg %.3f' % avg_score)
+    print("time: ", end_time - start_time)
